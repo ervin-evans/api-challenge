f8d20dbc0bbf66634c860054dc6b622918288683
Ervin Triano <ervintdeveloper@gmail.com>
2021-11-04 16:13:21 -0600

    Creacion del proyecto y conexión a la base de datos

---
+++ src/test/java/com/ervin/dev/ApiChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ervin.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
